b8e6471a4a4b0abdde85e51d2e5975ef9ed96b0c
bmacer <bmacer@cisco.com>
2024-12-23 12:24:20 -0500

    try to fix timeout

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import { ExpressAdapter } from "@nestjs/platform-express";
 import express from "express";
 
 const server = express();
+let cachedApp: express.Express;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
-  return app.getHttpAdapter().getInstance();
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    await app.init();
+    cachedApp = app.getHttpAdapter().getInstance();
+  }
+  return cachedApp;
 }
 
-// Export handler for Vercel
-export default bootstrap();
+// Convert to serverless handler format
+export default async function handler(req: any, res: any) {
+  const app = await bootstrap();
+  return app(req, res);
+}

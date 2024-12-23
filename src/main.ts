ff8303066cddb2dc72a458acf1ced36fc70bc0fc
bmacer <bmacer@cisco.com>
2024-12-23 13:14:22 -0500

    forget vercel

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
+// src/main.ts
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ExpressAdapter } from "@nestjs/platform-express";
 import express from "express";
 
-// Global Express instance
 const server = express();
+let cachedApp: express.Express;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.init();
-  return server;
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}`);
+  });
+
+  return cachedApp;
 }
 
-// Cache the server instance
-const serverPromise = bootstrap();
-
-// Vercel handler
-export default async function handler(req: any, res: any) {
-  const serverInstance = await serverPromise;
-  serverInstance(req, res); // Let Express handle the request
-}
+bootstrap();
